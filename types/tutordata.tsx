7739e09fc2c01d30d3cfd9f0948b9429aae7821d
wguo7 <150313145+wguo7@users.noreply.github.com>
2024-02-05 20:45:57 -0600

    Add files via upload

--- types/tutordata.tsx
+++ types/tutordata.tsx
@@ -1,1 +1,19 @@
+export type TutorData = {
+    "lastName": string, 
+    "firstName": string,
+    "aboutMe": string,
+    "id": number,
+    "hall": number,
+    "wing": string,
+    "graduationYear": number,
+    "emailAddress": string,
+    "mathcore": string[] | null,
+    "moreMath": string[] | null,
+    "physics": string[] | null,
+    "biology": string[] | null,
+    "chemistry": string[] | null,
+    "cs": string[] | null,
+    "language": string[] | null,
+    "otherScience": string[] | null,
+}
 
---
+++ types/weeklyAvailability.tsx
@@ -0,0 +1,9 @@
+export type WeeklyAvailability = {
+    "Sunday": string[],
+    "Monday": string[],
+    "Tuesday": string[],
+    "Wednesday": string[],
+    "Thursday": string[],
+    "Friday": string[],
+    "Saturday": string[],
+}
