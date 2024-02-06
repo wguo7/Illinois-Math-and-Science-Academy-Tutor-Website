8faf3947c31d9fe143058df8de1f5e3026976c23
wguo7 <150313145+wguo7@users.noreply.github.com>
2024-02-05 20:11:49 -0600

    Add files via upload

--- public/generics.js
+++ public/generics.js
@@ -1,1 +1,22 @@
-g
+export default {
+    data(){
+      return {
+        isMobile: null,
+      }
+    },
+    created (){
+      this.checkScreenSize();
+      window.addEventListener("resize", this.checkScreenSize);
+    },
+    methods: {
+      checkScreenSize(){
+        const constWidth = window.innerWidth;
+        if (constWidth <= 750){
+          this.isMobile = true;
+          return;
+        }
+  
+        this.isMobile = false;
+      }
+    }
+  }
